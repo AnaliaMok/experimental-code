6b01f5b85d57ebd3e15d8677be102649f82a01e3
Analia Mok <Analia Mok>
2017-03-22 16:48:44 -0400

    Added onclick functionality to menu button. Need to make menu appear above other content

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -3,3 +3,30 @@
  * Description: Javascript code used by test.html
  * @author: Analia Mok
  */
+
+
+/**
+ * menuDrop - onclick method for revealing and 
+ *  hiding the menu
+ */
+function menuDrop(){
+    // All a tags inside menu
+    var menuItems = document.getElementsByClassName("main_nav")[0].getElementsByTagName("a");
+    
+    var totalItems = menuItems.length;
+    
+    // Toggle all menu items based on value of display
+    for(var i = 0; i < totalItems; i++){
+        var currItem = menuItems[i];
+        
+        if(currItem.style.display === "none"){
+            // Toggle On
+            currItem.style.display = "block";
+        }else{
+            // Toggle Off
+            currItem.style.display = "none";
+        }
+        
+    }
+    
+} // End of menuDrop
